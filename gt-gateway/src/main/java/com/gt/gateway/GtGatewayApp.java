6cac81cf158910218153a46ba2b5e29b6ea853d0
SuHeng Qi <826504621@qq.com>
2022-12-26 07:51:42 +0800

    修改swagger

--- gt-gateway/src/main/java/com/gt/gateway/GtGatewayApp.java
+++ gt-gateway/src/main/java/com/gt/gateway/GtGatewayApp.java
@@ -2,11 +2,20 @@ package com.gt.gateway;
 
 import com.gt.gateway.loader.DynamicRouteLoader;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * @description: GtGatewayApp
@@ -27,10 +36,22 @@ public class GtGatewayApp implements CommandLineRunner {
 
     /**
      * 容器初始化后加载路由
+     *
      * @param args
      */
     @Override
     public void run(String... args) throws Exception {
         dynamicRouteLoader.refresh();
+    }
+
+    /**
+     * 接口地址（通过9999端口直接访问）
+     *
+     * @param indexHtml
+     * @return
+     */
+    @Bean
+    public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/META-INF/resources/doc.html") final org.springframework.core.io.Resource indexHtml) {
+        return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).syncBody(indexHtml));
     }
 }
---
+++ gt-gateway/src/main/java/com/gt/gateway/provider/SwaggerResourceController.java
@@ -0,0 +1,41 @@
+package com.gt.gateway.provider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger.web.*;
+
+import java.util.List;
+
+/**
+ * @description: swagger聚合接口，三个接口都是 doc.html需要访问的接口
+ * @date: 2022/12/25 14:45
+ * @author: qisuheng
+ */
+@RestController
+@RequestMapping("/swagger-resources")
+public class SwaggerResourceController {
+    private SwaggerProvider swaggerProvider;
+
+    @Autowired
+    public SwaggerResourceController(SwaggerProvider swaggerProvider) {
+        this.swaggerProvider = swaggerProvider;
+    }
+
+    @RequestMapping(value = "/configuration/security")
+    public ResponseEntity<SecurityConfiguration> securityConfiguration() {
+        return new ResponseEntity<>(SecurityConfigurationBuilder.builder().build(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/configuration/ui")
+    public ResponseEntity<UiConfiguration> uiConfiguration() {
+        return new ResponseEntity<>(UiConfigurationBuilder.builder().build(), HttpStatus.OK);
+    }
+
+    @RequestMapping
+    public ResponseEntity<List<SwaggerResource>> swaggerResources() {
+        return new ResponseEntity<>(swaggerProvider.get(), HttpStatus.OK);
+    }
+}
